57b5cf902e1401eb0bd3b8da568e02004efb5b61
Ryan Detzel <ryandetzel@gmail.com>
2015-06-08 08:31:06 -0400

    added proper logging. made more constants for the pin locations

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import logging
 from socket import *
 from threading import Thread
 import thread
@@ -15,14 +16,39 @@ from astral import Astral
 # Record how long it takes to open the door, close
 # ERror states
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('log.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
 class Door(object):
     MAX_MANUAL_MODE_TIME = 60 * 60
+    TIMEZONE_CITY = 'Boston'
     AFTER_SUNSET_DELAY = 30
     IDLE = UNKNOWN = NOT_TRIGGERED = AUTO = 0
     UP = OPEN = TRIGGERED = MANUAL = 1
     DOWN = CLOSED = 2
 
     PIN_LED = 5
+    PIN_BUTTON_UP = 13
+    PIN_BUTTON_DOWN = 19
+    PIN_SENSOR_TOP = 20
+    PIN_SENSOR_BOTTOM = 21
+    PIN_MOTOR_ENABLE = 18
+    PIN_MOTOR_A = 12
+    PIN_MOTOR_B = 16
 
     def __init__(self):
         self.door_status = Door.UNKNOWN
@@ -31,8 +57,8 @@ class Door(object):
         self.manual_mode_start = 0
 
         a = Astral()
-        self.city = a["Boston"]
-        self.setup_pins()
+        self.city = a[Door.TIMEZONE_CITY]
+        self.setupPins()
 
         t1 = Thread(target = self.checkInputs)
         t2 = Thread(target = self.checkTime)
@@ -49,17 +75,15 @@ class Door(object):
         serversocket.bind(addr)
         serversocket.listen(2)
 
-        # Get door position
         self.changeDoorMode(Door.AUTO)
         self.stopDoor(0)
 
-        ## Setup buttons
-        GPIO.add_event_detect(13, GPIO.RISING, callback=self.buttonPress, bouncetime=200)
-        GPIO.add_event_detect(19, GPIO.RISING, callback=self.buttonPress, bouncetime=200)
+        GPIO.add_event_detect(Door.PIN_BUTTON_UP, GPIO.RISING, callback=self.buttonPress, bouncetime=200)
+        GPIO.add_event_detect(Door.PIN_BUTTON_DOWN, GPIO.RISING, callback=self.buttonPress, bouncetime=200)
 
         while True:
             try:
-                print "Server is listening for connections\n"
+                logger.info("Server is listening for connections\n")
          
                 clientsocket, clientaddr = serversocket.accept()
                 thread.start_new_thread(handler, (clientsocket, clientaddr))
@@ -67,90 +91,94 @@ class Door(object):
                 break
             time.sleep(0.01)
 
-        print "Close connection"
+        logger.info("Close connection")
         GPIO.output(Door.PIN_LED, GPIO.LOW)
         serversocket.close()
         self.stopDoor(0)
 
-    def setup_pins(self):
+    def setupPins(self):
         GPIO.setmode(GPIO.BCM)
 
-        GPIO.setup(18, GPIO.OUT)
-        GPIO.setup(12, GPIO.OUT)
-        GPIO.setup(16, GPIO.OUT)
-        GPIO.setup(Door.PIN_LED, GPIO.OUT)  #LED
-        GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(Door.PIN_MOTOR_ENABLE, GPIO.OUT)
+        GPIO.setup(Door.PIN_MOTOR_A, GPIO.OUT)
+        GPIO.setup(Door.PIN_MOTOR_B, GPIO.OUT)
+        GPIO.setup(Door.PIN_LED, GPIO.OUT) 
+        GPIO.setup(Door.PIN_SENSOR_BOTTOM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(Door.PIN_SENSOR_TOP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(Door.PIN_BUTTON_UP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(Door.PIN_BUTTON_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def closeDoor(self):
         (top, bottom) = self.currentInputStatus()
         if (bottom == Door.TRIGGERED):
-            print "Door is already closed"
+            logger.info("Door is already closed")
             return
-        print "Closing door"
-        GPIO.output(18, GPIO.HIGH)
-        GPIO.output(12, GPIO.LOW)
-        GPIO.output(16, GPIO.HIGH)
+        logger.info("Closing door")
+        GPIO.output(Door.PIN_MOTOR_ENABLE, GPIO.HIGH)
+        GPIO.output(Door.PIN_MOTOR_A, GPIO.LOW)
+        GPIO.output(Door.PIN_MOTOR_B, GPIO.HIGH)
         self.direction = Door.DOWN
 
     def openDoor(self):
         (top, bottom) = self.currentInputStatus()
         if (top == Door.TRIGGERED):
-            print "Door is already open"
+            logger.info("Door is already open")
             return
-        print "Opening door"
-        GPIO.output(18, GPIO.HIGH)
-        GPIO.output(12, GPIO.HIGH)
-        GPIO.output(16, GPIO.LOW)
+        logger.info("Opening door")
+        GPIO.output(Door.PIN_MOTOR_ENABLE, GPIO.HIGH)
+        GPIO.output(Door.PIN_MOTOR_A, GPIO.HIGH)
+        GPIO.output(Door.PIN_MOTOR_B, GPIO.LOW)
         self.direction= Door.UP
 
     def stopDoor(self, delay):
-        print "Stop door"
-        time.sleep(delay)
-        GPIO.output(18, GPIO.LOW)
-        GPIO.output(12, GPIO.LOW)
-        GPIO.output(16, GPIO.LOW)
-        self.direction = Door.IDLE
+        if self.direction != Door.IDLE:
+            logger.info("Stop door")
+            time.sleep(delay)
+            GPIO.output(Door.PIN_MOTOR_ENABLE, GPIO.LOW)
+            GPIO.output(Door.PIN_MOTOR_A, GPIO.LOW)
+            GPIO.output(Door.PIN_MOTOR_B, GPIO.LOW)
+            self.direction = Door.IDLE
 
         (top, bottom) = self.currentInputStatus()
         if (top == Door.TRIGGERED):
-            print "Door is open"
+            logger.info("Door is open")
             self.door_status = Door.OPEN
         elif (bottom == Door.TRIGGERED):
-            print "Door is closed"
+            logger.info("Door is closed")
             self.door_status = Door.CLOSED
         else:
+            logger.info("Door is in an unknown state")
             self.door_status = Door.UNKNOWN
 
     def checkTime(self):
         while True:
             if self.door_mode == Door.AUTO:
-                current = datetime.datetime.now(pytz.timezone('US/Eastern'))
+                current = datetime.datetime.now(pytz.timezone(self.city.timezone))
                 sun = self.city.sun(date=datetime.datetime.now(), local=True)
 
                 after_sunset = sun["sunset"] + datetime.timedelta(minutes = Door.AFTER_SUNSET_DELAY)
 
                 if (current < sun["sunrise"] or current > after_sunset) and self.door_status != Door.CLOSED and self.direction != Door.DOWN:
-                    print "Door should be closed"
+                    logger.info("Door should be closed based on time")
                     self.closeDoor()
                 elif current > sun["sunrise"] and current < after_sunset and self.door_status != Door.OPEN and self.direction != Door.UP:
-                    print "Door should be open"
+                    logger.info("Door should be open based on time")
                     self.openDoor()
             time.sleep(1)
 
     def currentInputStatus(self):
-        bottom = GPIO.input(21)
-        top = GPIO.input(20)
+        bottom = GPIO.input(Door.PIN_SENSOR_BOTTOM)
+        top = GPIO.input(Door.PIN_SENSOR_TOP)
         return (top, bottom)
 
     def checkInputs(self):
         while True:
             (top, bottom) = self.currentInputStatus()
             if (self.direction == Door.UP and top == Door.TRIGGERED):
+                logger.info("Top sensor triggered")
                 self.stopDoor(0)
             if (self.direction == Door.DOWN and bottom == Door.TRIGGERED):
+                logger.info("Bottom sensor triggered")
                 self.stopDoor(1)
             time.sleep(0.01)
 
@@ -160,18 +188,17 @@ class Door(object):
             time.sleep(1)
             GPIO.output(Door.PIN_LED, GPIO.HIGH)
             time.sleep(1)
-            print (time.time()) - self.manual_mode_start
             if int(time.time()) - self.manual_mode_start > Door.MAX_MANUAL_MODE_TIME:
-                print "In manual mode too long"
+                logger.info("In manual mode too long, switching")
                 self.changeDoorMode(Door.AUTO)
 
     def changeDoorMode(self, new_mode):
         if new_mode == Door.AUTO: 
-            print "Enter auto mode"
+            logger.info("Entered auto mode")
             self.door_mode = Door.AUTO
             GPIO.output(Door.PIN_LED, GPIO.HIGH)
         else:
-            print "Enter manual mode"
+            logger.info("Entered manual mode")
             self.door_mode = Door.MANUAL
             self.stopDoor(0)
             
@@ -182,33 +209,32 @@ class Door(object):
             t2.start()
             
     def buttonPress(self, button):
-        print "Starting button..."
-        start = end = int(round(time.time() * 1000))
         waiting = True
+        start = end = int(round(time.time() * 1000))
+
         while GPIO.input(button) and waiting:
             end = int(round(time.time() * 1000))
-            diff = end - start
-            if (diff >= 2000):
-                waiting = False
-                if (self.door_mode == Door.AUTO):
+            if end - start >= 2000:
+                if self.door_mode == Door.AUTO:
                     self.changeDoorMode(Door.MANUAL)
                 else:
                     self.changeDoorMode(Door.AUTO)
+                time.sleep(2)
+                waiting = False
                 return
             time.sleep(0.1)
-        print "Ending"
      
         # Quick touch, what mode?
-        if (self.door_mode == Door.MANUAL):
-            if (self.direction != Door.IDLE):
+        if self.door_mode == Door.MANUAL:
+            if self.direction != Door.IDLE:
                 self.stopDoor(0)
-            elif (button == 13):
+            elif (button == Door.PIN_BUTTON_UP):
                 self.openDoor()
             else:
                 self.closeDoor()
 
     def handler(self, clientsocket, clientaddr):
-        print "Accepted connection from: ", clientaddr
+        logger.info("Accepted connection from: ", clientaddr)
      
         while True:
             data = clientsocket.recv(1024)
